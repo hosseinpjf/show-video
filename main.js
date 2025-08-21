e1b74b7f9424d3b60233ad7e11901834e8697c75
Hossein <hpourjafarabadi.dev@gmail.com>
2025-08-21 17:31:15 +0330

    The first website launch was successful.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-document.querySelector('video source').src = `./videos/video-${random()}.mp4`;
+const video = document.querySelector('video source');
 
 function random(){
     let number = Math.floor((Math.random() * 20) + 1);
-    console.log(number);
-    return number
+    return number;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    video.src = `./videos/video-${random()}.mp4`;
+})
